20bf9ad576b104330658b898e5c7d2817e2ff421
YORI212 <noteme98102@gmail.com>
2025-05-17 10:30:30 +0545

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,12 @@ def download():
 
     try:
         ydl_opts = {
-            'quiet': True,
-            'skip_download': True,
-            'format': 'best[ext=mp4]/best',
-        }
+    'quiet': True,
+    'skip_download': True,
+    'format': 'best[ext=mp4]/best',
+    'cookiefile': 'cookies.txt'  # 👈 Add this line
+}
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
             formats = []
